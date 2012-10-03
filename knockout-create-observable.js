f6229db17c0883d4f0237731247f404a0187276a
alekzonder <alekzonder@gmail.com>
2012-10-03 16:55:20 +0700

    fix comments and test observable

--- knockout-create-observable.js
+++ knockout-create-observable.js
@@ -1,14 +1,13 @@
 (function (factory) {
-    // Module systems magic dance.
-
+    // Module systems magic dance. (from ko.mapping)
     if (typeof require === "function" && typeof exports === "object" && typeof module === "object") {
-        // CommonJS or Node: hard-coded dependency on "knockout"
+        
         factory(require("knockout"), exports);
     } else if (typeof define === "function" && define["amd"]) {
-        // AMD anonymous module with hard-coded dependency on "knockout"
+        
         define(["knockout", "exports"], factory);
     } else {
-        // <script> tag: use the global `ko` object, attaching a `mapping` property
+        
         factory(ko, ko.advanced = {});
     }
 }(function (ko, exports) {
@@ -62,7 +61,7 @@
 
                     } else {
                         if (elements.length > 1) {
-                            if (!iterationContext[elem]) {
+                            if (!iterationContext()[elem]) {
                                 iterationContext()[elem] = ko.observable();
                             }
                             result = iterationContext()[elem];
